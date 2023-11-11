7189b7d190467c26dd9bb8a1ac159deef5cc20c2
avto <avtochoga123@gmail.com>
2023-11-11 18:50:27 +0400

    shualeduri

---
+++ index.js
@@ -0,0 +1,16 @@
+// const a = 1;
+// const b = 4;
+// const c = 3;
+
+// const discriminant = (b**2 - (4*a*c)) 
+// console.log( discriminant)
+ 
+// if(discriminant > 0 ){
+//     console.log('d > 0')
+// }
+// else if(discriminant < 0){
+//     console.log('d < 0')
+// }
+// else {
+//     console.log( 'd = 0')
+// }
