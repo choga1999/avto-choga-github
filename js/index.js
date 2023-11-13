a5d249f1ad4b49a2cbbdaba360711d8c5527ee08
avto <avtochoga123@gmail.com>
2023-11-13 12:43:44 +0400

    js file

---
+++ js/index.js
@@ -0,0 +1,37 @@
+
+const age = 15
+
+if (age >= 18) {
+    console.log('მომხმარებლის ასაკი არის 18 ან ზემოთ')
+   } else {
+       console.log('მომხმარებლის ასაკი არის 18-ზე ქვემოთ')
+   }
+   
+   
+   
+   
+   const weekDay = 7;
+   
+   switch (weekDay) {
+       case 1:
+         console.log("Sunday")
+         break;
+         case 2:
+           console.log("Monday")
+           break;
+           case 3:
+             console.log("Tuesday")
+             break;
+             case 4:
+               console.log("Wednesday")
+               break;
+               case 5:
+                   console.log("Thursday")
+                   break;
+                   case 6:
+                       console.log("Friday")
+                       break;
+                       case 7:
+                           console.log("Saturday")
+                           break;
+   }
